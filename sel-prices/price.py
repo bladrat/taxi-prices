525eeb08ed187b605e116a72cc9faeb9c10cf923
Bladrat <bladrat@yandex.ru>
2021-11-11 07:37:05 +0400

    main

--- sel-prices/price.py
+++ sel-prices/price.py
@@ -4,20 +4,33 @@ import time
 from fake_useragent import UserAgent
 
 
-def yandex_taxi(from_where, where):
-
-    url = "https://taxi.yandex.ru/"
-
-    #Настраиваем вебдрайвер
+def webdriver_options(proxy, path, headless):
+    
+    #Настраиваем вебдрайвер прописываем прокси, путь к драйверу и режим хеадлесс
     options = webdriver.ChromeOptions()
     options.add_argument("user-agent={useragent.random}")
-    options.add_argument('http://' + '85.26.146.169')
-
+    options.add_argument(proxy)
+    
     #тихий режим работы
-    #options.add_argument('headless')
+    if headless == 1:
+        options.add_argument('headless')
 
-    #Указываем путь к chromedriver
-    driver = webdriver.Chrome(executable_path=r"D:\\projects\\chromedriver\\chromedriver.exe", options=options)
+    return  webdriver.Chrome(executable_path=path, options=options)
+
+def send_addres(driver, adres, xpath_textarea, xpath_clean, xpath_select):
+
+    otkuda = driver.find_element(By.XPATH, xpath_textarea)
+    otkuda.send_keys(adres)
+    if xpath_clean != 0:
+        driver.find_element(By.XPATH, xpath_clean).click()
+    otkuda.send_keys(adres)
+    time.sleep(2)
+    driver.find_element(By.XPATH, xpath_select).click()
+
+def yandex_taxi(from_where, where):
+    url = "https://taxi.yandex.ru/"
+
+    driver = webdriver_options('http://85.26.146.169', r"D:\\projects\\chromedriver\\chromedriver.exe", 0)
 
     try:
         #Переход по ссылке
@@ -32,22 +45,18 @@ def yandex_taxi(from_where, where):
             k = 0
 
         #Находим поле ввода откуда, очищаем его и отправляем туда наше местоположение
-        otkuda = driver.find_element(By.XPATH,'//*[@id="application"]/div[1]/div[2]/div[1]/div[4]/div[2]/div[1]/div/div[1]/div/div[1]/div[1]/div[2]/span[1]/span[2]/textarea')
+        oxp_tx = '//*[@id="application"]/div[1]/div[2]/div[1]/div[4]/div[2]/div[1]/div/div[1]/div/div[1]/div[1]/div[2]/span[1]/span[2]/textarea'
+        oxp_cl = '//*[@id="application"]/div[1]/div[2]/div[1]/div[4]/div[2]/div[1]/div/div[1]/div/div[1]/div[1]/div[2]/span[1]/span[1]'
+        oxp_sl = '//*[@id="xuniq-0-1"]'
+        send_addres(driver, from_where, oxp_tx, oxp_cl, oxp_sl)
         
-        otkuda.send_keys(from_where)
-        driver.find_element(By.XPATH, '//*[@id="application"]/div[1]/div[2]/div[1]/div[4]/div[2]/div[1]/div/div[1]/div/div[1]/div[1]/div[2]/span[1]/span[1]').click()
-       
-        otkuda.send_keys(from_where)
-        time.sleep(2)
-        driver.find_element(By.XPATH,'//*[@id="xuniq-0-1"]').click()
+        kxp_tx = '//*[@id="application"]/div[1]/div[2]/div[1]/div[4]/div[2]/div[1]/div/div[1]/div/div[1]/div[2]/div[2]/span[1]/span[2]/textarea'
+        kxp_sl = '//*[@id="xuniq-0-27"]'
+        send_addres(driver, from_where, kxp_tx, 0, kxp_sl)
+
         
 
-        #Находим поле ввода куда, очищаем его и отправляем туда наше местоположение
-        kuda = driver.find_element(By.XPATH,'//*[@id="application"]/div[1]/div[2]/div[1]/div[4]/div[2]/div[1]/div/div[1]/div/div[1]/div[2]/div[2]/span[1]/span[2]/textarea')
-                                            
-        kuda.send_keys(from_where)
-        time.sleep(2)
-        driver.find_element(By.XPATH,'//*[@id="xuniq-0-27"]').click()
+
 
         time.sleep(999)
 
