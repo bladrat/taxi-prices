cff231c3ad2abfa9e95b422aa0f9c4af7d88eac1
Bladrat <bladrat@yandex.ru>
2021-11-19 17:10:29 +0400

    main

--- sel-prices/citimobil.py
+++ sel-prices/citimobil.py
@@ -1,7 +1,11 @@
-
 import requests
-
-def get_price():
+from geopy.geocoders import Nominatim
+ 
+def get_geo():
+    otkuda = 'Голосова 83 Тольятти'
+    kuda = 'Мира 101 Тольятти'
+    return otkuda, kuda
+def get_price(otkuda, kuda):
     headers = {
         'authority': 'widget.city-mobil.ru',
         'sec-ch-ua': '"Chromium";v="94", "Yandex";v="21", ";Not A Brand";v="99"',
@@ -17,9 +21,20 @@ def get_price():
         'referer': 'https://city-mobil.ru/',
         'accept-language': 'ru,en;q=0.9',
     }
-
-    data = '{"method":"getprice","ver":"4.59.0","phone_os":"widget","os_version":"web mobile-web","locale":"ru","latitude":55.780914,"longitude":37.631883,"del_latitude":55.739529,"del_longitude":37.621865,"options":[],"payment_type":["cash"],"tariff_group":[2,4,13,7,5],"source":"O","hurry":1}'
-
+ 
+    data = '{"method":"getprice","ver":"4.59.0","phone_os":"widget","os_version":"web mobile-web","locale":"ru","latitude":' + str(otkuda[0]) + ',"longitude":'+ str(otkuda[1])+ ',"del_latitude":'+ str(kuda[0])+ ',"del_longitude":'+ str(kuda[1])+ ',"options":[],"payment_type":["cash"],"tariff_group":[2,4,13,7,5],"source":"O","hurry":1}'
+    print(data)
     response = requests.post('https://widget.city-mobil.ru/c-api', headers=headers, data=data)
-
+ 
     print(response.text)
+ 
+def get_kord(address):
+    geolocator = Nominatim(user_agent = 'my_request')
+    otkuda_loc = geolocator.geocode(address)
+ 
+    return otkuda_loc.latitude, otkuda_loc.longitude
+ 
+ 
+a = 4
+a= get_price(get_kord('Голосова 83 Тольятти'), get_kord('Мира 101 Тольятти'))
+print(a)
